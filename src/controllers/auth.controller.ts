cb06e02ee8ec8ce18ff9332dfddaae5ef562281a
Mike Godfrinne <mike.godfrinne@gmail.com>
2026-02-25 09:17:10 +0100

    add argon2 to hash password and delete passport

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from "express";
+import argon2 from "argon2";
 import authService from "../services/auth.service.js";
 import UserModel from "../models/user.model.js";
 
@@ -16,15 +17,23 @@ const authController = {
         if (!userChecked) {
             return res.status(500).json({ error: "Failed to create user" });
         }
-        
-        const { email, password} = userChecked;
-        const user = await authService.signUp(email, password);
 
+        const { email, password} = userChecked;
+
+        let hashPassword;
+        try {
+            hashPassword = await argon2.hash(password)
+        } catch (error) {
+            return res.status(500).json({ error: "Password Hash failed"})
+        }
+        
+        const user = await authService.signUp(email, hashPassword);
 
         return res.status(201).json(user);
     },
 
-    login: (req: Request, res: Response) => {        
+    login: (req: Request, res: Response) => {
+
         return res.status(200).send("Login");
     }
 }
