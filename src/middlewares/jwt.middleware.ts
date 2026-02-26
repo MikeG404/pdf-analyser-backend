52f6955ec1f0938f4083425edea951c28f899521
Mike Godfrinne <mike.godfrinne@gmail.com>
2026-02-26 07:05:57 +0100

    Add TypeScript types for passport and jwt middleware

--- src/middlewares/jwt.middleware.ts
+++ src/middlewares/jwt.middleware.ts
@@ -1,13 +1,13 @@
 import passport from 'passport';
-import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
+import { Strategy as JwtStrategy, ExtractJwt, type StrategyOptions, type VerifiedCallback } from "passport-jwt";
 import authService from '../services/auth.service.js';
 
-const options = {
+const options: StrategyOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.JWT_SECRET
+    secretOrKey: process.env.JWT_SECRET as string
 }
 
-passport.use(new JwtStrategy(options, async (jwt_payload, done) => {
+passport.use(new JwtStrategy(options, async (jwt_payload: any, done: VerifiedCallback) => {
     try {
         const user = await authService.findUserByEmail(jwt_payload.email);
 
