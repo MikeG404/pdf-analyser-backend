73c661e98a4964aa55b7ecaabac0bc3bffe41736
Mike Godfrinne <mike.godfrinne@gmail.com>
2026-02-27 07:26:49 +0100

    Finish and test Google Auth

--- src/middlewares/jwt.middleware.ts
+++ src/middlewares/jwt.middleware.ts
@@ -1,6 +1,9 @@
 import passport from 'passport';
 import { Strategy as JwtStrategy, ExtractJwt, type StrategyOptions, type VerifiedCallback } from "passport-jwt";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import authService from '../services/auth.service.js';
+import { db } from '../index.js';
+import { usersTable } from '../db/schema.js';
 
 const options: StrategyOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -20,5 +23,43 @@ passport.use(new JwtStrategy(options, async (jwt_payload: any, done: VerifiedCal
         return done (error, false);
     }
 }))
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID as string,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    callbackURL: "/api/auth/google/callback" as string
+},
+    async (accessToken, refreshToken, profile, done) => {
+        try {
+            const email = profile.emails?.[0]?.value
+
+            if (!email) {
+                return done(new Error("No email found in Google Profile"), false);
+            };
+
+            let user = await authService.findUserByEmail(email);
+
+            if (!user) {
+                const [newUser] = await db.insert(usersTable)
+                    .values({email: email, password: null})
+                    .returning();
+
+                if (!newUser) {
+                    return done(null, false)
+                }
+
+                user = newUser;
+            }
+
+            if (!user) {
+                return done(null, false);   
+            }
+
+            return done(null, user);
+        } catch (error) {
+            return done(error, false);
+        }
+    }
+))
 
 export default passport;
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -6,6 +6,33 @@ const authRouter = express.Router();
 
 authRouter.post("/login", authController.login);
 authRouter.post("/signup", authController.signUp);
+
+// GOOGLE PROVIDER
+authRouter.get("/google", passport.authenticate('google', {
+    scope: ['profile', 'email'],
+    session: false
+}))
+
+authRouter.get('/google/callback', passport.authenticate('google', { 
+        session: false, 
+        failureRedirect: '/login-failed'
+    }),
+    (req, res) => {
+        const user = req.user;        
+
+        if (!user) {
+            return res.status(404).json({
+                message: "Connection failed!"
+            })
+        }
+
+        return res.status(200).json({
+            message: "Connection Succeed!",
+            user: user
+        })
+    }
+);
+
 authRouter.get("/profile", passport.authenticate('jwt', {session: false}),
     (req, res) => {
         res.status(200).json({message: "Carré VIP", user: req.user})
