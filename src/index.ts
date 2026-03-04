e0d902ff2295ba4a6af38bc8c235222d2693f71e
Mike Godfrinne <mike.godfrinne@gmail.com>
2026-03-04 09:22:47 +0100

    Add cors package

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { drizzle } from "drizzle-orm/node-postgres";
 
 import authRouter from "./routes/auth.route.js";
@@ -17,6 +18,7 @@ export const db = drizzle({
 })
 
 app.use(express.json());
+app.use(cors());
 app.use(morgan('dev'));
 
 app.use("/api/auth", authRouter);
